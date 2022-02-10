6a6090904910d86b043b909602bdd473d2913771
JustForCoding <mrme9093@gmail.com>
2022-02-10 12:17:11 +0200

    commit

--- main.go
+++ main.go
@@ -5,13 +5,14 @@ import (
 
 	"github.com/JustForCodin/chatv2/config"
 	"github.com/JustForCodin/chatv2/message"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
 	config := config.GetConfig()
 
-	dbClient, err := gorm.Open(config.MysqlDSN)
+	dbClient, err := gorm.Open(mysql.Open(config.MysqlDSN))
 	if err != nil {
 		panic(err)
 	}
